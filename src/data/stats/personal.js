778110e17c651332173a4d3edc5c4efbfe76e35d
thyang0119 <th.yang.0119@gmail.com>
2024-09-20 01:17:24 -0700

    remove-comment

--- src/data/stats/personal.js
+++ src/data/stats/personal.js
@@ -24,12 +24,6 @@ const data = [
     label: 'Current age',
     value: <Age />,
   },
-  // {
-  //   key: 'countries',
-  //   label: 'Countries visited',
-  //   value: 53,
-  //   link: 'https://www.google.com/maps/d/embed?mid=1iBBTscqateQ93pWFVfHCUZXoDu8&z=2',
-  // },
   {
     key: 'location',
     label: 'Current city',
